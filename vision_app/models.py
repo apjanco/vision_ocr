853aef2123160bf66e8c39693babe8d3f016efe0
root <root@ds-web.haverford.edu>
2017-11-17 06:16:29 -0500

    added new languages

--- vision_app/models.py
+++ vision_app/models.py
@@ -4,9 +4,28 @@ from django.core.files.storage import FileSystemStorage
 # Create your models here.
 class vision(models.Model):
 	RUSSIAN = 'Ru'
+	AZERBAIJANI = 'az'
+	BELARUSIAN = 'be'
+	BULGARIAN = 'bg'
+	CROATIAN = 'hr'	
+	CZECH = 'cs'
+	ESTONIAN = 'et'	
+	HUNGARIAN = 'hu'
+	KAZAKH = 'kk'
+	KYRGYZ = 'ky'
+	LATVIAN = 'lv'
+	LITHUANIAN = 'lt'
+	MACEDONIAN = 'mk'		
+	POLISH = 'pl'
+	ROMANIAN = 'ro'
+	SERBIAN = 'sr'
+	SLOVAK = 'sk'
+	SLOVENIAN = 'sl'		
 	SPANISH = 'Es'
+	UKRAINIAN = 'uk'
+	UZBEK = 'uz'
 	NONE = '-'
-	LANGUAGE_CHOICES = ((NONE, '-'),(RUSSIAN, 'Ru'), (SPANISH, 'Es'))
+	LANGUAGE_CHOICES = ((NONE, '-'),(RUSSIAN, 'Russian'), (AZERBAIJANI, 'Azerbaijani'), (BELARUSIAN, 'Belarusian'),(BULGARIAN, 'Bulgarian'),(CROATIAN,'Croatian'),(CZECH, 'Czech'),(ESTONIAN, 'Estonian'),(HUNGARIAN,'Hungarian'),(KAZAKH,'Kazakh'),(KYRGYZ,'Kyrgyz'),(LATVIAN, 'Latvian'),(LITHUANIAN,'Lithuanian'),(MACEDONIAN, 'Macedonian'),(POLISH, 'Polish'),(ROMANIAN, 'Romanian'),(SERBIAN, 'Serbian'),(SLOVAK, 'Slovak'),(SLOVENIAN, 'Slovenian'),(SPANISH, 'Spanish'),(UKRAINIAN, 'Ukrainian'),(UZBEK, 'Uzbek'))
 
 	language = models.CharField(max_length = 5, choices=LANGUAGE_CHOICES, default=NONE)
 	
